77464969a2d6839eea2d5a27ee9f65fb24b9b969
Andrei <andrei.vupt@doberstein.com.br>
2023-09-15 18:08:36 -0300

    rotas http de crud

--- src/server.js
+++ src/server.js
@@ -7,5 +7,23 @@ const dotenv = require('dotenv').config();
 // Definir a porta do servidor
 const PORT = process.env.PORT || 3005;
 
+// Criando rota get
+app.get('/api', (request, response) => {
+    response.send('Retorno de informações do banco de dados');
+});
+
+app.post('/api', (request, response) => {
+    response.send('Método utilizado para salvar informações!');
+});
+
+app.put('/api/:id', (request, response) => {
+    response.send('Método utilizado para editar informações!');
+});
+
+app.delete('/api/:id', (request, response) => {
+    response.send('Método utilizado para delete informações!');
+});
+
+
 // Testar servidor
 app.listen(PORT, () => console.log(`Running at port ${PORT}`));
